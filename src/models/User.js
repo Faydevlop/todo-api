c0faacc5df3d49734d4ae03f44126ca61e8d7f2c
fayis <fayfayis74@gmail.com>
2025-06-21 12:31:07 +0530

    final commit - tested with postman

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    email : {type:String,required:true,unique:true,index:true},
+    email : {type:String,required:true,unique:true,index:true,index: true },
     passwordHash : {type:String,required:true},
     createdAt:{type:Date,default:Date.now}
 })
